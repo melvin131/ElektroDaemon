302b2cf4703806d7876a8a78d7f93b3f8374f581
melvin <melvin.3349@gmail.com>
2018-02-03 13:13:36 +0100

    Added test code

---
+++ lib/ElektroDaemon/node.rb
@@ -0,0 +1,24 @@
+module ElektroDaemon
+    class Node < Client
+      attr_reader :node
+  
+      def initialize(node)
+        super(node)
+        @node = node
+      end
+  
+      def online?
+        begin
+          response = get("/")
+  
+          if response.success?
+            return true
+          else
+            return false
+          end
+        rescue
+          return false
+        end
+      end
+    end
+  end
